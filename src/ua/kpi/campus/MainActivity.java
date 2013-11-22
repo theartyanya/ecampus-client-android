39b9b1b1c2a40377dc833cd1c66c46a08120292c
Serhii Hokhalenko <Serhii Hokhalenko@77.47.218.116>
2013-11-23 01:39:05 +0200

    trial

--- src/ua/kpi/campus/MainActivity.java
+++ src/ua/kpi/campus/MainActivity.java
@@ -11,7 +11,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
-//hello commit Serge
+//hello commit Sergedsdsd
 	private EditText firstNumber;
 	private EditText secondNumber;
 	private Button sumButton;
