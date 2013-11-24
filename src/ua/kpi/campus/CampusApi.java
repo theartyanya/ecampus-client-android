cd17e97d16bc8048109c71130e08700c9c80b959
artur.dzidzoiev <artur.dzidzoiev@gmail.com>
2013-11-24 23:18:47 +0200

    Login() works correctly, test passed successfull Javadoc added

--- src/ua/kpi/campus/CampusApi.java
+++ src/ua/kpi/campus/CampusApi.java
@@ -5,56 +5,95 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import java.net.URL;
 
 import android.util.Log;
 
+/**
+ * Main Campus API
+ * 
+ * @author Artur Dzidzoiev
+ * @version Nov 24, 2013
+ * @see http://sdrv.ms/1gUsc17
+ * @see http://dev.ecampus.kpi.ua/library/item/campus-api-for-mobile
+ */
 public class CampusApi {
+	/**
+	 * API Campus link
+	 */
 	private final static String API_URL = "http://api.ecampus.kpi.ua/";
 
+	/**
+	 * Logging in to Campus system
+	 * 
+	 * @return JSON - string
+	 */
 	public static String login(String login, String password) {
 		String parameters = String.format("User/Auth?login=%s&password=%s",
 				login, password);
-		URL url;
+		;
 		HttpURLConnection connection;
-		OutputStreamWriter request;
 		String response = "";
 
 		try {
-			url = new URL(API_URL + parameters);
-			connection = (HttpURLConnection) url.openConnection();
-			connection.setDoOutput(true);
-			connection.setRequestProperty("Content-Type",
-					"application/x-www-form-urlencoded");
-			// set the request method to GET
-			connection.setRequestMethod("GET");
-			// get the output stream from the connection you created
-			request = new OutputStreamWriter(connection.getOutputStream());
-			// write your data to the ouputstream
-			request.write(parameters);
-			request.flush();
-			request.close();
-			String line = "";
-			// create your inputsream
-			InputStreamReader isr = new InputStreamReader(
-					connection.getInputStream());
-			// read in the data from input stream, this can be done a variety of
-			// ways
-			BufferedReader reader = new BufferedReader(isr);
-			StringBuilder sb = new StringBuilder();
-			while ((line = reader.readLine()) != null) {
-				sb.append(line + "\n");
-			}
-			// get the string version of the response data
-			response = sb.toString();
-			// do what you want with the data now
-
-			// always remember to close your input and output streams
-			isr.close();
-			reader.close();
+			URL url = CreateURL(API_URL, parameters);
+			connection = openConnectionGet(url);
+			sendRequest(parameters, connection);
+			response = getLine(connection);
 		} catch (IOException e) {
 			Log.e("HTTP GET:", e.toString());
 		}
+
 		return response;
+	}
+
+	private static String getLine(HttpURLConnection connection)
+			throws IOException {
+		String response;
+		String receivedLine = "";
+		// create your input stream
+		InputStreamReader isr = new InputStreamReader(
+				connection.getInputStream());
+		// read in the data from input stream, this can be done a variety of
+		// ways
+		BufferedReader reader = new BufferedReader(isr);
+		StringBuilder sb = new StringBuilder();
+		while ((receivedLine = reader.readLine()) != null) {
+			sb.append(receivedLine + "\n");
+		}
+		response = sb.toString();
+		isr.close();
+		reader.close();
+		return response;
+	}
+
+	private static void sendRequest(String parameters,
+			HttpURLConnection connection) throws IOException {
+		OutputStreamWriter request;
+		// get the output stream from the connection you created
+		request = new OutputStreamWriter(connection.getOutputStream());
+		// write your data to the output stream
+		request.write(parameters);
+		request.flush();
+		request.close();
+	}
+
+	private static HttpURLConnection openConnectionGet(URL url)
+			throws IOException, ProtocolException {
+		HttpURLConnection connection;
+		connection = (HttpURLConnection) url.openConnection();
+		connection.setDoOutput(true);
+		connection.setRequestProperty("Content-Type",
+				"application/x-www-form-urlencoded");
+		// set the request method to GET
+		connection.setRequestMethod("GET");
+		return connection;
+	}
+
+	private static URL CreateURL(String API_URL, String parameters)
+			throws MalformedURLException {
+		return new URL(API_URL + parameters);
 	}
 }
