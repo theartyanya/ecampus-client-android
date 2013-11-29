b698ad128c083b61dad8bf02c6df6971c14d33c4
artur.dzidzoiev <artur.dzidzoiev@gmail.com>
2013-11-29 02:22:46 +0200

    Permissions now are immutable

--- src/ua/kpi/campus/jsonparsers/Permissions.java
+++ src/ua/kpi/campus/jsonparsers/Permissions.java
@@ -9,58 +9,47 @@ import java.util.ArrayList;
  * @author Serhii Hokhalenko
  * @version 24 Nov 2013
  */
-class Permissions {
-	private int statusCode;
-	private Timestamp timeStamp;
-	private String guid;
-	private Object padding;
-	private ArrayList<GetPermissionsData> data;
+public  class Permissions {
+	private final int statusCode;
+	private final Timestamp timeStamp;
+	private final String guid;
+	private final Object padding;
+	private final ArrayList<GetPermissionsData> data;
 	
 	
 	public Permissions(int statusCode, Timestamp timeStamp, String guid,
                        Object padding, ArrayList<GetPermissionsData> data) {
-		super();
 		this.statusCode = statusCode;
 		this.timeStamp = timeStamp;
 		this.guid = guid;
 		this.padding = padding;
 		this.data = data;
 	}
-	int getStatusCode() {
+
+    @Override
+    public String toString() {
+        return "GetPermissions [statusCode=" + statusCode + ", timeStamp="
+                + timeStamp + ", guid=" + guid + ", padding=" + padding
+                + ", data=" + data + "]";
+    }
+
+    public int getStatusCode() {
 		return statusCode;
 	}
-	void setStatusCode(int statusCode) {
-		this.statusCode = statusCode;
-	}
-	@Override
-	public String toString() {
-		return "GetPermissions [statusCode=" + statusCode + ", timeStamp="
-				+ timeStamp + ", guid=" + guid + ", padding=" + padding
-				+ ", data=" + data + "]";
-	}
-	Timestamp getTimeStamp() {
+
+    public Timestamp getTimeStamp() {
 		return timeStamp;
 	}
-	void setTimeStamp(Timestamp timeStamp) {
-		this.timeStamp = timeStamp;
-	}
-	String getGuid() {
+
+	public String getGuid() {
 		return guid;
 	}
-	void setGuid(String guid) {
-		this.guid = guid;
-	}
-	Object getPadding() {
+
+	public Object getPadding() {
 		return padding;
 	}
-	void setPadding(Object padding) {
-		this.padding = padding;
-	}
-	ArrayList<GetPermissionsData> getData() {
+
+	public ArrayList<GetPermissionsData> getData() {
 		return data;
 	}
-	void setData(ArrayList<GetPermissionsData> data) {
-		this.data = data;
-	}
-	
 }
