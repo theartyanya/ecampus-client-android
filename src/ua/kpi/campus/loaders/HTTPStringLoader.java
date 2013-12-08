c4f39bac3c1ae748d35a90c9e59693e72c6a01bd
artur.dzidzoiev <artur.dzidzoiev@gmail.com>
2013-12-09 00:05:50 +0200

    Creation new activity and showing Toast with info

--- src/ua/kpi/campus/loaders/HTTPStringLoader.java
+++ src/ua/kpi/campus/loaders/HTTPStringLoader.java
@@ -6,21 +6,20 @@ import android.util.Log;
 
 
 /**
- * Class
+ * Load strings from HTTP using asyncTask
  *
  * @author Artur Dzidzoiev
  * @version 12/7/13
  */
 public class HttpStringLoader extends AsyncTaskLoader<HttpResponse> {
     public final static String URL_STRING = "URL";
-    private final String URL_ADDRESS;
     public final static String LOG_TAG = HttpStringLoader.class.getName();
+    private final String URL_ADDRESS;
 
     public HttpStringLoader(Context context, String URL_ADDRESS) {
         super(context);
         Log.d(HttpStringLoader.class.getName(), " loader created");
         this.URL_ADDRESS = URL_ADDRESS;
-        onContentChanged();
     }
 
     @Override
@@ -31,9 +30,9 @@ public class HttpStringLoader extends AsyncTaskLoader<HttpResponse> {
 
     @Override
     protected void onStartLoading() {
-        Log.d(LOG_TAG, hashCode() + " onStartLoading");
-        if (takeContentChanged())
-            forceLoad();
+            Log.d(LOG_TAG, hashCode() + " onStartLoading");
+            if (takeContentChanged())
+                forceLoad();
     }
 
     @Override
@@ -42,17 +41,16 @@ public class HttpStringLoader extends AsyncTaskLoader<HttpResponse> {
         cancelLoad();
     }
 
-
     @Override
     protected void onAbandon() {
         super.onAbandon();
         Log.d(LOG_TAG, hashCode() + " onAbandon");
     }
 
-
     @Override
     protected void onReset() {
         super.onReset();
+        onStopLoading();
         Log.d(LOG_TAG, hashCode() + " onReset");
     }
 
