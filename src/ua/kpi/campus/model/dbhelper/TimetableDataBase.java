691c48eac7d2f99e5e343d3eafbaa5bbea8829bf
artur.dzidzoiev <artur.dzidzoiev@gmail.com>
2013-12-25 11:45:06 +0200

    getting all lessons

--- src/ua/kpi/campus/model/dbhelper/TimetableDataBase.java
+++ src/ua/kpi/campus/model/dbhelper/TimetableDataBase.java
@@ -2,9 +2,13 @@ package ua.kpi.campus.model.dbhelper;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.util.Log;
 import ua.kpi.campus.model.TimetableElement;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Class
@@ -27,9 +31,39 @@ public class TimetableDataBase extends DatabaseHelper{
         values.put(KEY_TIMETABLE_BUILDING, timetableElement.getBuilding());
         values.put(KEY_TIMETABLE_EMPLOYEE_PHOTO_PATH, timetableElement.getEmployeePhotoPath());
         values.put(KEY_TIMETABLE_WEEKEND_NUM, timetableElement.getWeekNum());
-        values.put(KEY_TIMETABLE_DAY_ID, timetableElement.getDayName());
+        values.put(KEY_TIMETABLE_DAY_ID, timetableElement.getDayId());
         values.put(KEY_TIMETABLE_DAY_NAME, timetableElement.getDayName());
         Log.d(TAG, hashCode() + " adding message id" + timetableElement.getLessonId());
         return (int) db.insert(TABLE_TIMETABLE, null, values);
     }
+
+    public List<TimetableElement> getAllLessons() {
+        List<TimetableElement> subjects = new ArrayList<>();
+        String selectQuery = "SELECT * FROM " + TABLE_TIMETABLE +
+                " ORDER BY " + KEY_TIMETABLE_DAY_ID;
+        Log.d(TAG, hashCode() + " SQL query: " + selectQuery);
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor c = db.rawQuery(selectQuery, null);
+
+        if(c.getCount() == 0) {
+            return subjects;
+        }
+
+        if (c.moveToFirst()) {
+            do {
+                TimetableElement timetableElement = new TimetableElement(
+                        c.getInt(c.getColumnIndex(KEY_TIMETABLE_LESSON_ID)),
+                        c.getString(c.getColumnIndex(KEY_TIMETABLE_EMPLOYEE)),
+                        c.getString(c.getColumnIndex(KEY_TIMETABLE_SUBJECT)),
+                        c.getString(c.getColumnIndex(KEY_TIMETABLE_BUILDING)),
+                        c.getString(c.getColumnIndex(KEY_TIMETABLE_EMPLOYEE_PHOTO_PATH)),
+                        c.getInt(c.getColumnIndex(KEY_TIMETABLE_WEEKEND_NUM)),
+                        c.getInt(c.getColumnIndex(KEY_TIMETABLE_DAY_ID)),
+                        c.getString(c.getColumnIndex(KEY_TIMETABLE_DAY_NAME)));
+                        subjects.add(timetableElement);
+            } while (c.moveToNext());
+        }
+        return subjects;
+    }
+
 }
