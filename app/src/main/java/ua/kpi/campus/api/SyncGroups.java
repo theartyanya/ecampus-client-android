cb821dd92e926cbbc85458b2301ef4fcebb3570c
Artyom Dorosh <doroshartyom@gmail.com>
2015-01-18 15:59:31 +0200

    Latest commit before first nightly build

---
+++ app/src/main/java/ua/kpi/campus/api/SyncGroups.java
@@ -0,0 +1,16 @@
+package ua.kpi.campus.api;
+
+import android.content.Context;
+
+/**
+ * Created by doroshartyom on 18.01.2015.
+ */
+public class SyncGroups {
+
+    public static final String BASE_URL = "http://api.rozklad.org.ua/v1/groups/?q=";
+    private static final String LOG_TAG = "SyncGroups";
+
+    private static Context context;
+    
+    
+}
