6a00d736b337db87de42cee75f2508a3127e2532
Davyd Melkon <david_melkon@ukr.net>
2015-03-17 20:08:14 +0200

    Notes are working but in one area!

--- app/src/main/java/ua/kpi/campus/ui/notes/SecondActivity.java
+++ app/src/main/java/ua/kpi/campus/ui/notes/SecondActivity.java
@@ -1,7 +1,11 @@
 package ua.kpi.campus.ui.notes;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import ua.kpi.campus.R;
 
@@ -10,9 +14,39 @@ import ua.kpi.campus.R;
  */
 public class SecondActivity extends ActionBarActivity {
 
+    private Button create;
+    private Button cancel;
+    private EditText name;
+    private EditText description;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.notes_new);
+
+        create = (Button) findViewById(R.id.okBtn);
+        cancel = (Button) findViewById(R.id.cancelBtn);
+        name = (EditText) findViewById(R.id.notesNameText);
+        description = (EditText) findViewById(R.id.notesDescriptionText);
+
+        create.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SecondActivity.this, SocialActivity.class);
+
+                intent.putExtra("notesName", name.getText().toString());
+                intent.putExtra("notesDescription", description.getText().toString());
+
+                startActivity(intent);
+            }
+        });
+
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SecondActivity.this, SocialActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/ua/kpi/campus/ui/notes/SocialActivity.java
+++ app/src/main/java/ua/kpi/campus/ui/notes/SocialActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import ua.kpi.campus.R;
 
@@ -13,19 +14,33 @@ import ua.kpi.campus.R;
  */
 public class SocialActivity extends ActionBarActivity {
 
+    private Button newNote;
+    private TextView name;
+    private TextView description;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.notes_main);
 
-        Button btn = (Button) findViewById(R.id.newNote);
-        btn.setOnClickListener(new View.OnClickListener() {
+        newNote = (Button) findViewById(R.id.newNote);
+        name = (TextView) findViewById(R.id.nametxt);
+        description = (TextView) findViewById(R.id.descriptiontxt);
+
+        newNote.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(SocialActivity.this, SecondActivity.class);
                 startActivity(intent);
             }
         });
+
+        String txtName = getIntent().getStringExtra("notesName");
+        String txtDescription = getIntent().getStringExtra("notesDescription");
+
+        name.setText(name.getText().toString() + " " + txtName);
+        description.setText(description.getText().toString() + " " + txtDescription);
+
 
     }
 
