c15f86ab843ff73f30ac37fc408f7b5bd99a053b
floring <floring@example.com>
2016-01-26 15:44:02 +0200

    Implement ActivityContext annotation to improve context injection

---
+++ app/src/main/java/com/kpi/campus/di/ActivityContext.java
@@ -0,0 +1,20 @@
+package com.kpi.campus.di;
+
+import java.lang.annotation.Retention;
+
+import javax.inject.Qualifier;
+
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+/**
+ * Annotation created to improve Context injection. This annotation is used with @Inject for
+ * Context class to return the current Activity context.
+ *
+ * This annotation can be replaced with a @Named annotation, but configure before the
+ * ActivityModule.
+ * Created by Administrator on 26.01.2016.
+ */
+@Qualifier
+@Retention(RUNTIME)
+public @interface ActivityContext {
+}
