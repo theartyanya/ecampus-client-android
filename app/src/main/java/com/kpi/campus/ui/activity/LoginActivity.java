c3f667c8dfd926eca8ccefa10adfacfe4a7ff69d
floring <floring@example.com>
2016-01-28 13:23:28 +0200

    Change package of MainActivity and LoginActivity

--- app/src/main/java/com/kpi/campus/ui/activity/LoginActivity.java
+++ app/src/main/java/com/kpi/campus/ui/activity/LoginActivity.java
@@ -1,7 +1,9 @@
-package com.kpi.campus;
+package com.kpi.campus.ui.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.kpi.campus.R;
 
 public class LoginActivity extends AppCompatActivity {
 
