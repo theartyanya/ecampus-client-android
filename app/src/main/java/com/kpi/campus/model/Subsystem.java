4626e3f87c0bbeea4e76d1da88587de90319aa28
floring <floring@example.com>
2016-01-29 14:59:22 +0200

    Implement Subsystem class with its manager

---
+++ app/src/main/java/com/kpi/campus/model/Subsystem.java
@@ -0,0 +1,18 @@
+package com.kpi.campus.model;
+
+import android.content.Context;
+
+/**
+ * Created by Administrator on 28.01.2016.
+ */
+public class Subsystem {
+    private String mName;
+
+    public Subsystem(String name) {
+        mName = name;
+    }
+
+    public String getName() {
+        return mName;
+    }
+}
