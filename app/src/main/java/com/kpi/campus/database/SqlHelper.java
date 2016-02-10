99bf05c94593f3b63773ba7188d6bfe4bccbee77
floring <floring@example.com>
2016-02-10 16:55:37 +0200

    Implement database SqlHelper class

---
+++ app/src/main/java/com/kpi/campus/database/SqlHelper.java
@@ -0,0 +1,37 @@
+package com.kpi.campus.database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.net.Uri;
+
+import com.kpi.campus.database.table.TokenTable;
+
+/**
+ * Created by Administrator on 09.02.2016.
+ */
+public class SqlHelper extends SQLiteOpenHelper {
+
+    public static final String CONTENT_AUTHORITY = "com.kpi.campus.database.contentprovider";
+
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    private static final String DATABASE_NAME = "api.db";
+
+    private static final int DATABASE_VERSION = 1;
+
+    public SqlHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL(TokenTable.Requests.CREATION_REQUEST);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL(TokenTable.Requests.DROP_REQUEST);
+        onCreate(sqLiteDatabase);
+    }
+}
