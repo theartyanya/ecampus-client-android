d87bb239d68b7e0b859e41cd73939c374c8472ad
floring <floring@example.com>
2016-02-15 16:07:17 +0200

    Fix auth retrofit interface method

--- app/src/main/java/com/kpi/campus/api/service/AuthService.java
+++ app/src/main/java/com/kpi/campus/api/service/AuthService.java
@@ -1,32 +1,18 @@
 package com.kpi.campus.api.service;
 
-import com.kpi.campus.model.Airport;
-import com.kpi.campus.model.LoginData;
-import com.kpi.campus.model.Token;
-
-import java.util.List;
+import com.kpi.campus.model.pojo.Token;
 
 import retrofit2.Call;
-import retrofit2.http.Body;
-import retrofit2.http.GET;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.POST;
-import retrofit2.http.Query;
 
 /**
  * Created by Administrator on 08.02.2016.
  */
 public interface AuthService {
 
-//    @Headers({
-//            "Accept: application/json",
-//            "Content-Type: application/json"
-//    })
+    @FormUrlEncoded
     @POST("/oauth/token")
-    Call<Token> auth(@Body LoginData loginData);
-
-    @GET("/places/coords_to_places_ru.json")
-    Call<List<Airport>> airports(@Query("coords") String gps);
-
-    @GET("/test/5")
-    Call<String> test();
+    Call<Token> auth(@Field("username") String email, @Field("password") String password, @Field("grant_type") String type);
 }
