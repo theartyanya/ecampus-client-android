ed7c2656d5b9c93e5283950420ea229f5033be9d
floring <floring@example.com>
2016-02-25 13:57:53 +0200

    Implement AutoCompleteView for selection of subdivision

---
+++ app/src/main/java/com/kpi/campus/model/Recipient.java
@@ -0,0 +1,17 @@
+package com.kpi.campus.model;
+
+/**
+ * Created by Administrator on 25.02.2016.
+ */
+public class Recipient {
+
+    private String mName;
+
+    public void setName(String name) {
+        mName = name;
+    }
+
+    public String getName() {
+        return mName;
+    }
+}
