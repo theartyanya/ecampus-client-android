5060be11f7b48b4cf1f1d2391a8b2f68932e4760
floring <floring@example.com>
2016-02-29 17:01:51 +0200

    Add LoginActivityTest class

---
+++ app/src/androidTest/java/com/kpi/campus/ui/LoginActivityTest.java
@@ -0,0 +1,35 @@
+package com.kpi.campus.ui;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+
+import com.kpi.campus.R;
+import com.kpi.campus.ui.activity.LoginActivity;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+/**
+ * Created by Administrator on 29.02.2016.
+ */
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class LoginActivityTest {
+
+    @Rule
+    ActivityTestRule<LoginActivity> mActivityRule = new ActivityTestRule<LoginActivity>(LoginActivity.class);
+
+    @Test
+    public void inputCredentials() {
+        onView(withId(R.id.edit_text_input_login)).perform(typeText("mylogin"), closeSoftKeyboard());
+        onView(withId(R.id.edit_text_input_password)).perform(typeText("mypassword"), closeSoftKeyboard());
+
+    }
+}
