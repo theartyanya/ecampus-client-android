81ddb0e3dca81d46302b48b7fbb82a04bfc17d91
floring <floring@example.com>
2016-03-01 14:25:23 +0200

    Add tests to LoginActivityTest class

--- app/src/androidTest/java/com/kpi/campus/ui/LoginActivityTest.java
+++ app/src/androidTest/java/com/kpi/campus/ui/LoginActivityTest.java
@@ -1,6 +1,6 @@
 package com.kpi.campus.ui;
 
-import android.support.test.rule.ActivityTestRule;
+import android.support.test.espresso.intent.rule.IntentsTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
 
@@ -12,8 +12,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.matcher.ComponentNameMatchers.hasShortClassName;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 import static android.support.test.espresso.matcher.ViewMatchers.isRoot;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static com.kpi.campus.custom.OrientationChangeAction.orientationLandscape;
@@ -26,18 +30,39 @@ import static com.kpi.campus.custom.OrientationChangeAction.orientationPortrait;
 @LargeTest
 public class LoginActivityTest {
 
-    @Rule
-    ActivityTestRule<LoginActivity> mActivityRule = new ActivityTestRule<LoginActivity>(LoginActivity.class);
+    private final String SHORT_CLASS_NAME = ".ui.activity.MainActivity";
 
+    @Rule
+    public IntentsTestRule<LoginActivity> mIntentsRule = new IntentsTestRule<>(
+            LoginActivity.class);
+
+    /**
+     * Tests that user can enter credentials
+     */
     @Test
-    public void inputCredentials() {
+    public void testInputCredentials() {
         onView(withId(R.id.edit_text_input_login)).perform(typeText("mylogin"), closeSoftKeyboard());
         onView(withId(R.id.edit_text_input_password)).perform(typeText("mypassword"), closeSoftKeyboard());
     }
 
+    /**
+     * Tests that screen is rotated without exceptions
+     */
     @Test
-    public void rotateScreen() {
+    public void testRotateScreen() {
         onView(isRoot()).perform(orientationLandscape());
         onView(isRoot()).perform(orientationPortrait());
+    }
+
+    /**
+     * Tests that correct intent is sent by clicking on a login button
+     */
+    @Test
+    public void testIntent() {
+        onView(withId(R.id.edit_text_input_login)).perform(typeText("mky"), closeSoftKeyboard());
+        onView(withId(R.id.edit_text_input_password)).perform(typeText("mky"), closeSoftKeyboard());
+        onView(withId(R.id.button_login)).perform(click());
+        // Asserts that the given component class name matches intent sent by the application under test.
+        intended(hasComponent(hasShortClassName(SHORT_CLASS_NAME)));
     }
 }
