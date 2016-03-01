d207dc40e67235767c2329d590e681a721e1ceca
floring <floring@example.com>
2016-03-01 12:43:00 +0200

    Add custom Espresso action OrientationChangeAction that changes the orientation of the screen

--- app/src/androidTest/java/com/kpi/campus/ui/LoginActivityTest.java
+++ app/src/androidTest/java/com/kpi/campus/ui/LoginActivityTest.java
@@ -14,7 +14,10 @@ import org.junit.runner.RunWith;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.matcher.ViewMatchers.isRoot;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static com.kpi.campus.custom.OrientationChangeAction.orientationLandscape;
+import static com.kpi.campus.custom.OrientationChangeAction.orientationPortrait;
 
 /**
  * Created by Administrator on 29.02.2016.
@@ -30,6 +33,11 @@ public class LoginActivityTest {
     public void inputCredentials() {
         onView(withId(R.id.edit_text_input_login)).perform(typeText("mylogin"), closeSoftKeyboard());
         onView(withId(R.id.edit_text_input_password)).perform(typeText("mypassword"), closeSoftKeyboard());
+    }
 
+    @Test
+    public void rotateScreen() {
+        onView(isRoot()).perform(orientationLandscape());
+        onView(isRoot()).perform(orientationPortrait());
     }
 }
