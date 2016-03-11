015cf31a47bc08758ccf48dd547c5b1faff393cd
floring <floring@example.com>
2016-03-11 15:17:20 +0200

    Add ConstraintsAction class which impose condition on action

---
+++ app/src/androidTest/java/com/kpi/campus/helper/action/ConstraintsAction.java
@@ -0,0 +1,33 @@
+package com.kpi.campus.helper.action;
+
+import android.support.test.espresso.UiController;
+import android.support.test.espresso.ViewAction;
+import android.view.View;
+
+import org.hamcrest.Matcher;
+
+/**
+ * Impose conditions on the implementation of actions.
+ * Created by Administrator on 11.03.2016.
+ */
+public class ConstraintsAction {
+
+    public static ViewAction withConstraints(final ViewAction action, final Matcher<View> constraints) {
+        return new ViewAction() {
+            @Override
+            public Matcher<View> getConstraints() {
+                return constraints;
+            }
+
+            @Override
+            public String getDescription() {
+                return action.getDescription();
+            }
+
+            @Override
+            public void perform(UiController uiController, View view) {
+                action.perform(uiController, view);
+            }
+        };
+    }
+}
