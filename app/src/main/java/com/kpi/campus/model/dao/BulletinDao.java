87bfa1dce4276bee2d9df98c9754215be86448b7
floring <floring@example.com>
2016-03-21 16:13:48 +0200

    Add BulletinDao data access object pattern

---
+++ app/src/main/java/com/kpi/campus/model/dao/BulletinDao.java
@@ -0,0 +1,39 @@
+package com.kpi.campus.model.dao;
+
+import com.kpi.campus.model.Bulletin;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Administrator on 21.03.2016.
+ */
+public class BulletinDao implements IDataAccessObject<Bulletin> {
+
+    private List<Bulletin> mBulletins = new ArrayList<>();
+
+    @Override
+    public List<Bulletin> getData() {
+        return mBulletins;
+    }
+
+    @Override
+    public void setData(List<Bulletin> data) {
+        mBulletins = data;
+    }
+
+    @Override
+    public Bulletin get(int number) {
+        return mBulletins.get(number);
+    }
+
+    @Override
+    public void update(Bulletin object) {
+
+    }
+
+    @Override
+    public void delete(Bulletin object) {
+
+    }
+}
