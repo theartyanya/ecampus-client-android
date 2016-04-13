f75f7660dae8c295ceab67ff8b94ab2677791fae
floring <floring@example.com>
2016-04-13 17:00:15 +0300

    Add subdivision, group, profile fields to the Recipient model

--- app/src/main/java/com/kpi/campus/model/Recipient.java
+++ app/src/main/java/com/kpi/campus/model/Recipient.java
@@ -9,11 +9,50 @@ public class Recipient {
 
     private String mName;
 
+    private String subdivisionId;
+
+    private String profileId;
+
+    private String studyGroupId;
+
+    public Recipient() {
+    }
+
+    public Recipient(String subdivId, String profileId, String groupId) {
+        this.subdivisionId = subdivId;
+        this.profileId = profileId;
+        this.studyGroupId = groupId;
+    }
+
     public void setName(String name) {
         mName = name;
     }
 
     public String getName() {
         return mName;
+    }
+
+    public String getSubdivisionId() {
+        return subdivisionId;
+    }
+
+    public void setSubdivisionId(String subdivisionId) {
+        this.subdivisionId = subdivisionId;
+    }
+
+    public String getProfileId() {
+        return profileId;
+    }
+
+    public void setProfileId(String profileId) {
+        this.profileId = profileId;
+    }
+
+    public String getStudyGroupId() {
+        return studyGroupId;
+    }
+
+    public void setStudyGroupId(String studyGroupId) {
+        this.studyGroupId = studyGroupId;
     }
 }
