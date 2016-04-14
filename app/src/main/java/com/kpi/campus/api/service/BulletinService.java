b8744f38713beb86c2b4b6ecd4a396f148b6c421
floring <floring@example.com>
2016-04-14 12:09:22 +0300

    Fix post request with POST annotation

--- app/src/main/java/com/kpi/campus/api/service/BulletinService.java
+++ app/src/main/java/com/kpi/campus/api/service/BulletinService.java
@@ -8,6 +8,7 @@ import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
 import retrofit2.http.Headers;
+import retrofit2.http.POST;
 import retrofit2.http.Query;
 import rx.Observable;
 
@@ -42,7 +43,7 @@ public interface BulletinService {
             "Accept: application/json",
             "Content-Type: application/json"
     })
-    @GET("/board/new")
+    @POST("/board/new")
     Observable<String> postNewBulletin(
             @Header("Authorization") String authorization,
             @Body Bulletin bulletin);
