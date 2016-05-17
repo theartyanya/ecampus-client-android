7b4f8fc6fb91c31d34c6d89fb75b2d44324486b0
floring <floring@users.noreply.github.com>
2016-05-17 16:52:13 +0300

    Fix getSubsystems() test

--- app/src/test/java/ua/kpi/campus/presenter/MainPresenterTest.java
+++ app/src/test/java/ua/kpi/campus/presenter/MainPresenterTest.java
@@ -1,6 +1,8 @@
 package ua.kpi.campus.presenter;
 
 import android.content.Context;
+import android.content.res.Resources;
+import android.content.res.TypedArray;
 
 import junit.framework.Assert;
 
@@ -11,6 +13,7 @@ import org.mockito.MockitoAnnotations;
 
 import java.util.List;
 
+import ua.kpi.campus.R;
 import ua.kpi.campus.model.Subsystem;
 import ua.kpi.campus.ui.Navigator;
 import ua.kpi.campus.ui.Preference;
@@ -19,6 +22,7 @@ import ua.kpi.campus.ui.presenter.MainPresenter;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 /**
  * Created by Admin on 17.05.2016.
@@ -34,6 +38,8 @@ public class MainPresenterTest {
     private Preference mPreference;
     @Mock
     private Context mContext;
+    @Mock
+    private Resources resources;
 
     @Before
     public void setUp() {
@@ -59,8 +65,17 @@ public class MainPresenterTest {
 
     @Test
     public void getSubsystems() {
+        int subsystemNumber = 6;
+        String[] names = new String[]{"1", "2", "3", "4", "5", "6"};
+        when(mContext.getResources()).thenReturn(resources);
+        when(mContext.getResources().getStringArray(R.array.full_subsystem)).thenReturn(names);
+        TypedArray img = mock(TypedArray.class);
+        for(int i = 0; i < subsystemNumber; i++)
+            when(img.getResourceId(i, -1)).thenReturn(-1);
+        when(resources.obtainTypedArray(R.array.full_subsystem_image)).thenReturn(img);
+
         List<Subsystem> list = mPresenter.getData();
-        Assert.assertEquals(6, list.size());
+        Assert.assertEquals(subsystemNumber, list.size());
     }
 
     @Test
