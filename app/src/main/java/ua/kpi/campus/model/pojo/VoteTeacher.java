d86f12750d68b86b93f4c5225c10ff1ce2d123f3
floring <floring@example.com>
2016-06-01 13:30:21 +0300

    Implement pojo classes for voting

---
+++ app/src/main/java/ua/kpi/campus/model/pojo/VoteTeacher.java
@@ -0,0 +1,80 @@
+package ua.kpi.campus.model.pojo;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Administrator on 01.06.2016.
+ */
+public class VoteTeacher {
+
+    @SerializedName("voteId")
+    @Expose
+    private String voteId;
+    @SerializedName("teacherId")
+    @Expose
+    private String teacherId;
+    @SerializedName("teacherName")
+    @Expose
+    private String teacherName;
+    @SerializedName("isVoted")
+    @Expose
+    private boolean isVoted;
+    @SerializedName("criterions")
+    @Expose
+    private List<Item> criterions = new ArrayList<>();
+    @SerializedName("avgResult")
+    @Expose
+    private String avgResult;
+
+    public String getVoteId() {
+        return voteId;
+    }
+
+    public void setVoteId(String voteId) {
+        this.voteId = voteId;
+    }
+
+    public String getTeacherId() {
+        return teacherId;
+    }
+
+    public void setTeacherId(String teacherId) {
+        this.teacherId = teacherId;
+    }
+
+    public String getTeacherName() {
+        return teacherName;
+    }
+
+    public void setTeacherName(String teacherName) {
+        this.teacherName = teacherName;
+    }
+
+    public boolean isVoted() {
+        return isVoted;
+    }
+
+    public void setIsVoted(boolean isVoted) {
+        this.isVoted = isVoted;
+    }
+
+    public List<Item> getCriterions() {
+        return criterions;
+    }
+
+    public void setCriterions(List<Item> criterions) {
+        this.criterions = criterions;
+    }
+
+    public String getAvgResult() {
+        return avgResult;
+    }
+
+    public void setAvgResult(String avgResult) {
+        this.avgResult = avgResult;
+    }
+}
---
+++ app/src/main/java/ua/kpi/campus/model/pojo/VoteTerm.java
@@ -0,0 +1,55 @@
+package ua.kpi.campus.model.pojo;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by Administrator on 01.06.2016.
+ */
+public class VoteTerm {
+
+    @SerializedName("voteId")
+    @Expose
+    private String voteId;
+    @SerializedName("voteName")
+    @Expose
+    private String voteName;
+    @SerializedName("dateStart")
+    @Expose
+    private String dateStart;
+    @SerializedName("dateStop")
+    @Expose
+    private String dateStop;
+
+    public String getVoteId() {
+        return voteId;
+    }
+
+    public void setVoteId(String voteId) {
+        this.voteId = voteId;
+    }
+
+    public String getVoteName() {
+        return voteName;
+    }
+
+    public void setVoteName(String voteName) {
+        this.voteName = voteName;
+    }
+
+    public String getDateStart() {
+        return dateStart;
+    }
+
+    public void setDateStart(String dateStart) {
+        this.dateStart = dateStart;
+    }
+
+    public String getDateStop() {
+        return dateStop;
+    }
+
+    public void setDateStop(String dateStop) {
+        this.dateStop = dateStop;
+    }
+}
